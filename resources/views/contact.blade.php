198e489c3bc549454dcef94062c4eb010bb30c07
RFNshare <rfnshare@gmail.com>
2020-07-08 16:10:12 +0600

    Working with dynamic view like yield, require, section

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,28 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <style>
-        .col-md-6
-        {
-            text-align: center;
-            color: darkblue;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col-12 col-md-6">
-            <h1>Contact Page</h1>
-        </div>
-    </div>
-</div>
-</body>
-</html>
+@section('content')
+
+    <h1>Contact Page</h1>
+
+@stop
+
+@section('footer')
+    <script>alert("Hello From Contact Page")
+    </script>
+
+@stop
