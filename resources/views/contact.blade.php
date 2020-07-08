c09d7e8fd2c2b01f9784e773fc7b8c142044606b
RFNshare <rfnshare@gmail.com>
2020-07-08 17:48:21 +0600

    Database Migration. Rollback, AddColumn etc

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,25 +1,35 @@
 @extends('layouts.app')
 
-
 @section('content')
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <h1>Contact Page</h1>
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <button type="button" class="btn btn-success">Success</button>
+            <button type="button" class="btn btn-danger">Danger</button>
+            <button type="button" class="btn btn-warning">Warning</button>
+            <button type="button" class="btn btn-info">Info</button>
+            <button type="button" class="btn btn-light">Light</button>
+            <button type="button" class="btn btn-dark">Dark</button>
 
-    <h1>Contact Page</h1>
+            <button type="button" class="btn btn-link">Link</button>
+        </div>
+        <div class="col-12 col-md-6">
+            @if(count($peoples))
+                <ul>
+                    @foreach($peoples as $person)
+                        <li>{{$person}}</li>
 
-    @if(count($peoples))
-
-        <ul>
-
-            @foreach($peoples as $person)
-                <li>{{$person}}</li>
-
-            @endforeach
-        </ul> 
-    @endif
-
+                    @endforeach
+                </ul>
+            @endif</div>
+    </div>
 @stop
 
+
 @section('footer')
-{{--    <script>alert("Hello From Contact Page")--}}
-{{--    </script>--}}
+    {{--    <script>alert("Hello From Contact Page")--}}
+    {{--    </script>--}}
 
 @stop
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
     <title>Document</title>
 </head>
 <body>
@@ -14,6 +15,7 @@
 
 @yield('footer')
 
+<script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
 
 </body>
 </html>
